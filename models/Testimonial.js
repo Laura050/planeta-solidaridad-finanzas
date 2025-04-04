8f519da98a794c019ac937143c9efa99f60b974e
Laura050 <nizbol72@gmail.com>
2025-04-04 16:43:23 +0200

    Create Testimonial.js

---
+++ models/Testimonial.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const TestimonialSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Testimonial', TestimonialSchema);
